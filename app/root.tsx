1bcbba9ab791d674dd735e55aee34577d80b0de2
Derek Scruggs <me@derekscruggs.com>
2024-08-01 18:11:56 -0500

    added user button to layout

--- app/root.tsx
+++ app/root.tsx
@@ -6,11 +6,11 @@ import {
   ScrollRestoration,
   useLoaderData
 } from "@remix-run/react";
-import { useState } from "react"
+import { useState ,useContext } from "react"
 import { LoaderFunction } from "@remix-run/node"
 import "./tailwind.css"
 import { rootAuthLoader } from "@clerk/remix/ssr.server"
-import { ClerkApp } from "@clerk/remix"
+import { ClerkApp , UserButton } from "@clerk/remix"
 import { CurrentUserContext } from "./utils/CurrentUserContext"
 import { type CurrentUser } from "~/models/types"
 import { getUserByClerkId } from "~/models/user.server"
@@ -31,15 +31,19 @@ export const loader: LoaderFunction = async args => {
 
 function App() {
   const { user } = useLoaderData<{ user: CurrentUser }>()
+  
   const [currentUser, setCurrentUser] = useState<CurrentUser | null>(user as CurrentUser)
   return (
     <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
-      <Outlet />
+      <Layout>
+        <Outlet />
+      </Layout>
     </CurrentUserContext.Provider>
   )
 }
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { currentUser } = useContext(CurrentUserContext)
   return (
     <html lang="en">
       
@@ -50,8 +54,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <div className="h-screen w-screen bg-slate-100">
-          {children}
+        <div className="relative h-screen w-screen bg-slate-100">
+          {currentUser &&
+          <div className="absolute right-4 top-4">
+            <UserButton/>
+          </div>
+          } 
+        {children}
         </div>
         <ScrollRestoration />
         <Scripts />
